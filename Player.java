ad3c0a68f06024bf0dfce7d2ab2f0a3c51c1809c
Viren Khandal <virenkhandal@berkeley.edu>
2021-07-05 16:00:38 -0700

    added class for properties

--- Player.java
+++ Player.java
@@ -3,8 +3,12 @@ import java.util.Random;
 // @author Viren Khandal
 public class Player{
     private Position position;
+    private int balance;
     public Position get_position(){
         return position;
+    }
+    public int get_balance(){
+        return balance;
     }
     public int roll_dice(){
         Random rand = new Random();
---
+++ Property.java
@@ -0,0 +1,15 @@
+// @author Viren Khandal
+public class Property{
+    private String name;
+    private int base_rent;
+    private String color;
+    public String get_name(){
+        return name;
+    }
+    public int get_base_rent(){
+        return base_rent;
+    }
+    public String get_color(){
+        return color;
+    }
+}
