e7e85dee10e26ddc87a89f76f5927de77e8bc92a
Viren Khandal <virenkhandal@berkeley.edu>
2021-07-05 15:58:02 -0700

    added dice roll function for player

--- Player.java
+++ Player.java
@@ -1,7 +1,14 @@
+import java.util.Random;
+
 // @author Viren Khandal
 public class Player{
     private Position position;
     public Position get_position(){
         return position;
+    }
+    public int roll_dice(){
+        Random rand = new Random();
+        int roll = rand.nextInt(11) + 2;
+        return roll;
     }
 }
